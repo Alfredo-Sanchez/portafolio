16ae162ec054296c6ae3d97027e793e038b7a39f
Alfredo Sanchez <alfredosanchez080114@gmail.com>
2024-10-29 13:31:50 -0300

    dark mode done and img tronsformed in svg

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function toogleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const toggleButton = document.querySelector(".toggle-dark-mode");
+toggleButton.addEventListener("click", toggleDarkMode);
+
+const toggleButton2 = document.querySelector(".toggle-dark-mode-2");
+toggleButton2.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode(event) {
+  event.preventDefault(); // Previene comportamientos no deseados en móviles
+  document.body.classList.toggle("dark-mode");
+}
